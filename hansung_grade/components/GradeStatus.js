740607f8a895edf9f105ee7fddc3709d9bd6b50b
PRESSO (via OpenClaw) <caff1nepill@gmail.com>
2026-03-05 11:27:58 +0000

    [OpenClaw] chore: Cleanup hansung_grade for sub-branch subtree

--- hansung_grade/components/GradeStatus.js
+++
@@ -1,7 +0,0 @@
-export const GradeStatus = () => {
-    return (
-        <div>
-            hi
-        </div>
-    )
-}
--- hansung_grade/utils/types.js
+++
@@ -1,5 +0,0 @@
-export const GraphType = {
-    GPA: "gpa",
-    MGPA: "mgpa",
-    BOTH: "both"
-}
